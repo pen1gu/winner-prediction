c3c6869ee24505ba843cbbcfa7f926a26285eca0
Joon <kkkths@naver.com>
2025-11-27 20:30:47 +0900

    init

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
-from server.app.crawler.player_info import PlayerInfoCrawler
+from server.app.crawler.fotmob import FotMobCrawler
+from server.app.models.player import Player
 import asyncio
 
+from server.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
 async def main():
-    crawler = PlayerInfoCrawler()
-    try:
-        await crawler.test_httpx()
-    finally:
-        await crawler.close()
+    crawler = FotMobCrawler()
+    players_info = await crawler.get_players_info_by_team_id(9825)
+
+    print(players_info)
 
 if __name__ == "__main__":
     asyncio.run(main())
