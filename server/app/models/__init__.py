4dd4b7ec292cd6f1f91db8b689a4905a27f9ef33
Joon <kkkths@naver.com>
2026-01-03 20:57:35 +0900

    feat : alembic table 호환성 관련 코드 수정

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -4,6 +4,8 @@ from server.app.models.teams.manager import Manager, ManagerBase
 from server.app.models.players.player_details import PlayerDetails, PlayerDetailsBase
 from server.app.models.players.player_match_affect_features import PlayerMatchAffectFeatures, PlayerMatchAffectFeaturesBase
 from server.app.models.bets.bet import Bets, BetsBase
+from server.app.models.matches.match_logs import MatchLogs, MatchLogsBase
+from server.app.models.matches.match_details import MatchDetails, MatchDetailsBase
 
 # 하위 호환성을 위한 re-export (기존 import와의 호환성)
 TeamModel = Team
@@ -25,6 +27,10 @@ __all__ = [
     "PlayerMatchAffectFeaturesBase",
     "Bets",
     "BetsBase",
+    "MatchLogs",
+    "MatchLogsBase",
+    "MatchDetails",
+    "MatchDetailsBase",
     # 하위 호환성
     "TeamModel",
     "PlayerModel",
